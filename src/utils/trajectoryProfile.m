bc1554d12221dae707954d55f1a07379ca920ad2
Soowon Kim <soowk311@gmail.com>
2021-05-27 05:04:49 +0900

    modified traj

--- src/utils/trajectoryProfile.m
+++ src/utils/trajectoryProfile.m
@@ -3,26 +3,29 @@
 % Pre acceleration
 pre = 10;
 % Theta; Specifies number of roll
-roll_num = 3;
+roll_num = 2;
 % Number of points on a circle
 pts_circle = 5;
 angle_0 = -pi/2;
 theta = linspace(angle_0, roll_num*2*pi + angle_0, roll_num*pts_circle+1);
 % Circle stride
-ds = linspace(0,10,length(theta));
+dx = linspace(0,10,length(theta));
 % Nominal radius
-r = 1;
+r = 2;
 % X,Y,Z
-x = r.*cos(theta) + ds;
+x = r.*cos(theta) + dx;
 y = linspace(pre,1*pre,length(theta));
-z = (r.*sin(theta)+r)*3;
+z = (r.*sin(theta)+r)*2;
 % Way points
-PATH = [x',y',z';pre/2+ds(end),y(end),0];
+PATH = [x',y',z';pre/2+dx(end),y(end),0];
 PATH = PATH+[pre/2, -y(1), 0];
 PATH = [zeros(1,3);PATH];
+% Stride along y axis
+dy = linspace(0,1,length(PATH));
+PATH(:,2,:) = PATH(:,2,:) + dy';
 % Time intervals
 Tau_vec = zeros(length(PATH)-1,1);
-Tau_vec(2:end-1) = 0.5;
+Tau_vec(2:end-1) = 0.75;
 Tau_vec(1) = 1.75;
 Tau_vec(end) = 1.75;
 
